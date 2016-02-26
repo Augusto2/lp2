082b0589ec4d14690b18d30f65a52fb036045222
Augusto2 <augusto_vga@hotmail.com>
2016-02-26 08:44:23 -0300

    Create Exercicio1_1.java
    Exercicio1_1

---
+++ exercicio1/Exercicio1_1.java
@@ -0,0 +1,12 @@
+public class Exercicio1_1 {
+	String nome;
+	public void imprimeNome(){
+		System.out.println("O meu nome eh Augus");
+	}
+
+
+public static void main(String [] args){
+	Exercicio1_1 e1 = new Exercicio1_1();
+		e1.imprimeNome();
+}
+}
